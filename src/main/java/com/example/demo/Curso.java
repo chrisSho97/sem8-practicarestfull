e239669a52614b07936fb3611a1fd91d1345d9de
chrisSho97 <134670235+chrisSho97@users.noreply.github.com>
2023-07-23 17:12:31 +0000

    actualizando

--- src/main/java/com/example/demo/Curso.java
+++ src/main/java/com/example/demo/Curso.java
@@ -1,16 +1,22 @@
 package com.example.demo;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
 @Entity
 @Table (name="curso")
 public class Curso {
 
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String nombre;
     private Integer creditos;
-    
+
     public Integer getId() {
         return id;
     }
