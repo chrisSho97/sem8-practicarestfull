ae466b222b65548eb12f4431504e842e9786b88f
chrisSho97 <134670235+chrisSho97@users.noreply.github.com>
2023-07-23 18:32:57 +0000

    actualizando2

--- src/main/java/com/example/demo/Curso.java
+++ src/main/java/com/example/demo/Curso.java
@@ -1,9 +1,12 @@
 package com.example.demo;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -16,6 +19,9 @@ public class Curso {
     private Integer id;
     private String nombre;
     private Integer creditos;
+    @ManyToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "id_carrera")
+    private Carrera carrera;
 
     public Integer getId() {
         return id;
@@ -34,6 +40,12 @@ public class Curso {
     }
     public void setCreditos(Integer creditos) {
         this.creditos = creditos;
+    }
+    public Carrera getCarrera() {
+        return carrera;
+    }
+    public void setCarrera(Carrera carrera) {
+        this.carrera = carrera;
     }
 
     
